64f772e7ad4bee0d16e155710a6ae479a2d09eca
Abraham Schneider <abe.schneider@gmail.com>
2014-07-23 07:29:52 -0400

    getting initial code into shape with some tests

--- src/GrammaticalEvolution.jl
+++ src/GrammaticalEvolution.jl
@@ -2,6 +2,9 @@ module GrammaticalEvolution
 
 using EBNF
 
+export Individual, Population
+export select_two_individuals, one_point_crossover, mutate!
+
 abstract Individual
 abstract Population
 
@@ -11,27 +14,80 @@ function evaluate!{PopulationType <: Population}(pop::PopulationType)
   end
 end
 
-function run{PopulationType <: Population}(size::Int64, generations::Int64)
-  pop = PopulationType(size)
+#function isless(x::Individual, y::Individual)
+#  return x
+#end
 
-  evaluate!(pop)
-  for i=1:generations
-    # create new population for this generation
-    gen = PopulationType(size)
+function sort!{PopulationType <: Population}(pop::PopulationType)
+  sort!(pop.individuals)
+end
 
-    # sort population
-    sort!(gen)
+function select_two_individuals{IndividualType <: Individual}(individuals::Array{IndividualType})
+  # randomly select two individuals from a population
+  while true
+    i1 = rand(1:length(individuals))
+    i2 = rand(1:length(individuals))
 
-    # take top %
-
-    # replicate top with 2-point cross-over
-
-    # mutate
-
-
-    # return new population
-
+    # make sure the two individuals are not the same
+    if i1 != i2
+      return (i1, i2)
+    end
   end
 end
+
+function one_point_crossover{IndividualType <: Individual}(ind1::IndividualType, ind2::IndividualType)
+  cross_point = rand(1:length(ind1))
+  g1 = vcat(ind1[1:cross_point-1], ind2[cross_point:end])
+  g2 = vcat(ind2[1:cross_point-1], ind1[cross_point:end])
+
+  return (IndividualType(g1), IndividualType(g2), cross_point)
+end
+
+function mutate!(ind::Individual, mutation_rate::Float64; max_value=1000)
+  for i=1:length(ind)
+    if rand() < mutation_rate
+      ind[i] = rand(1, max_value)
+    end
+  end
+end
+
+function generate{PopulationType <: Population}(population::PopulationType, top_percent::Float64, prob_mutation::Float64, mutation_rate::Float64)
+    # sort population
+    sort!(population)
+
+    # take top %
+    top_num = length(population.individuals)*top_percent
+    top_performers = population.individuals[1:top_num]
+
+    # create a new population
+    new_population = PopulationType(top_num)
+
+    # re-populate by mating top performers
+    while length(new_population.individuals) < size
+      (i1, i2) = select_two_individuals(pop)
+      (ind2, ind2) = one_point_crossover(top_performers)
+      push!(new_population, ind1)
+      push!(new_population, ind2)
+    end
+
+    # mutate
+    for j=1:size
+      if rand() < prob_mutation
+        mutate!(new_population.individuals[top_num:end], mutation_rate)
+      end
+    end
+
+  return new_population
+end
+
+# function run{PopulationType <: Population}(population::PopulationType, generations::Int64, top_percent::Float64, prob_mutation::Float64, mutation_rate::Float64)
+#   evaluate!(population)
+#   for i=1:generations
+#     new_population = generate(population, top_percent, prob_mutation, mutation_rate)
+
+#     # return new population
+#     produce(new_population)
+#   end
+# end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,61 @@
 using GrammaticalEvolution
 using Base.Test
+import Base.getindex
+import Base.length
+import Base.endof
 
-# write your own tests here
-@test 1 == 1
+type TestIndividual <: Individual
+  genome::Array{Int64, 1}
+  fitness::Float64
+
+  function TestIndividual(size::Int64, max_value::Int64)
+    genome = rand(1:max_value, size)
+    return new(genome, -1.0)
+  end
+
+  TestIndividual(genome::Array{Int64, 1}) = new(genome, -1.0)
+end
+
+type TestPopulation{IndividualType <: Individual} <: Population
+  individuals::Array{IndividualType, 1}
+
+  function TestPopulation(population_size::Int64, genome_size::Int64)
+    individuals = Array(IndividualType, 0)
+    for i=1:population_size
+      push!(individuals, IndividualType(genome_size, 1000))
+    end
+
+    return new(individuals)
+  end
+end
+
+length(pop::TestPopulation) = length(pop.individuals)
+getindex(pop::TestPopulation, index::Int64) = pop.individuals[index]
+
+length(ind::TestIndividual) = length(ind.genome)
+endof(ind::TestIndividual) = endof(ind.genome)
+getindex(ind::TestIndividual, indices...) = ind.genome[indices...]
+isless(ind1::TestIndividual, ind2::TestIndividual) = ind1.fitness < ind2.fitness
+
+# test creationg of population
+pop = TestPopulation{TestIndividual}(5, 10)
+@test length(pop) == 5
+@test length(pop[1]) == 10
+
+# test selecting two individuals
+for i=1:20
+  (i1, i2) = select_two_individuals(pop.individuals)
+  @test i1 != i2
+  @test i1 <= length(pop)
+  @test i2 <= length(pop)
+end
+
+# test crossing over two genomes
+(i1, i2) = select_two_individuals(pop.individuals)
+for i=1:20
+  (g1, g2, cross_point) = one_point_crossover(pop[i1], pop[i2])
+  @test g1[1:cross_point-1] == pop[i1].genome[1:cross_point-1]
+  @test g1[cross_point:end] == pop[i2].genome[cross_point:end]
+  @test g2[1:cross_point-1] == pop[i2].genome[1:cross_point-1]
+  @test g2[cross_point:end] == pop[i1].genome[cross_point:end]
+end
