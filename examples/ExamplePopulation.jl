18938ddcc0394896f4fdf4fd539d58a4face3837
Abraham Schneider <abe.schneider@gmail.com>
2014-08-15 10:23:40 -0400

    almost forgot to add ExamplePopulation

---
+++ examples/ExamplePopulation.jl
@@ -0,0 +1,25 @@
+type ExampleIndividual <: Individual
+  genome::Array{Int64, 1}
+  fitness::Float64
+  code
+
+  function ExampleIndividual(size::Int64, max_value::Int64)
+    genome = rand(1:max_value, size)
+    return new(genome, -1.0, nothing)
+  end
+
+  ExampleIndividual(genome::Array{Int64, 1}) = new(genome, -1.0, nothing)
+end
+
+type ExamplePopulation <: Population
+  individuals::Array{ExampleIndividual, 1}
+
+  function ExamplePopulation(population_size::Int64, genome_size::Int64)
+    individuals = Array(ExampleIndividual, 0)
+    for i=1:population_size
+      push!(individuals, ExampleIndividual(genome_size, 1000))
+    end
+
+    return new(individuals)
+  end
+end
